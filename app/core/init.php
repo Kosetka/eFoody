1400f71294ce36dede27e7f9715f19db6f927b75
Kosetka <kosetka.xx@gmail.com>
2024-05-14 12:02:37 +0200

    FIX

--- app/core/init.php
+++ app/core/init.php
@@ -3,9 +3,8 @@
 spl_autoload_register(function ($classname) {
 
 	require $filename = "../app/models/" . ucfirst($classname) . ".php";
-	show($filename);
 });
-die;
+
 require 'config.php';
 require 'functions.php';
 require 'Database.php';
