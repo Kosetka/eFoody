9598144ceef3cd889c4c0bdd9ad1b591590a0127
Mateusz Zybura <mateusz.zybura@gmail.com>
2024-05-29 21:05:18 +0200

    test

--- app/core/App.php
+++ app/core/App.php
@@ -17,31 +17,31 @@ class App
 	{
 		show($_SERVER);
 		die;
-		$URL = $this->splitURL();
-		/** select controller **/
-		$filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
-		if (file_exists($filename)) {
-			require $filename;
-			$this->controller = ucfirst($URL[0]);
-			//show($filename);die;
-			unset($URL[0]);
-		} else {
+		/*$URL = $this->splitURL();
+			  
+			  $filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
+			  if (file_exists($filename)) {
+				  require $filename;
+				  $this->controller = ucfirst($URL[0]);
+				  //show($filename);die;
+				  unset($URL[0]);
+			  } else {
 
-			$filename = "../app/controllers/_404.php";
-			require $filename;
-			$this->controller = "_404";
-		}
+				  $filename = "../app/controllers/_404.php";
+				  require $filename;
+				  $this->controller = "_404";
+			  }
 
-		$controller = new $this->controller;
-		/** select method **/
-		if (!empty($URL[1])) {
-			if (method_exists($controller, $URL[1])) {
-				$this->method = $URL[1];
-				unset($URL[1]);
-			}
-		}
+			  $controller = new $this->controller;
+			  
+			  if (!empty($URL[1])) {
+				  if (method_exists($controller, $URL[1])) {
+					  $this->method = $URL[1];
+					  unset($URL[1]);
+				  }
+			  }
 
-		call_user_func_array([$controller, $this->method], $URL);
+			  call_user_func_array([$controller, $this->method], $URL);*/
 
 	}
 
