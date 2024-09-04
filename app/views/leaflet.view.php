444d75f0b98542b25ce89d974d69e97bb349c586
Kosetka <kosetka.xx@gmail.com>
2024-09-04 11:49:01 +0200

    TEST

--- app/views/leaflet.view.php
+++ app/views/leaflet.view.php
@@ -466,7 +466,7 @@ foreach ($lines as $line) {
     }
     $y += $lineHeight-3;
 }
-
+/*
 // Nagłówek HTTP informujący przeglądarkę, że zwracany plik to obraz JPEG
 header('Content-Type: image/jpeg');
 
@@ -485,3 +485,4 @@ imagedestroy($po_name_new);
 imagedestroy($word_menu);
 imagedestroy($word_menu_new);
 imagedestroy($obraz);
+*/
