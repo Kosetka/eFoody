18793d9acf70dc9c67bb464f902b1b0a68a46334
Kosetka <kosetka.xx@gmail.com>
2025-02-07 15:42:47 +0100

    ADD

--- app/core/functions.php
+++ app/core/functions.php
@@ -473,3 +473,34 @@ function countDaysExcludingSundays($dateFrom, $dateTo, $holidays = []) {
 
     return $nonSundayAndHolidayCount;
 }
+
+function sms_send($params, $token, $backup = false)
+{
+    static $content;
+
+    if ($backup == true) {
+        $url = 'https://api2.smsapi.pl/sms.do';
+    } else {
+        $url = 'https://api.smsapi.pl/sms.do';
+    }
+
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $url);
+    curl_setopt($c, CURLOPT_POST, true);
+    curl_setopt($c, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, array(
+        "Authorization: Bearer $token"
+    ));
+
+    $content = curl_exec($c);
+    $http_status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+
+    if ($http_status != 200 && $backup == false) {
+        $backup = true;
+        sms_send($params, $token, $backup);
+    }
+
+    curl_close($c);
+    return $content;
+}
